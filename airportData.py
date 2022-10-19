db17a8fc13e3b653f1d8a9d045ba8c52eedcf448
Aiden-Jacobs <aiden192@gmail.com>
2022-10-18 21:06:07 -0700

    to access airport Data

---
+++ airportData.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+class Airport_Look_UP:
+    def __init__(self, filename):
+        self.file= pd.read_csv(filename)
+        self.file.columns = ["id","ident","type","name","latitude_deg","longitude_deg","elevation_ft","continent","country_name","iso_country","region_name","iso_region","local_region","municipality","scheduled_service","gps_code","iata_code","local_code","home_link","wikipedia_link","keywords","score","last_updated"]
+
+    def getLatlong(self,iata_code):
+        for i in range(self.file.index.size):
+            if self.file.loc[i,"iata_code"] == iata_code:
+                return(self.file.loc[i,"latitude_deg"],self.file.loc[i,"longitude_deg"])
+
+    pass
+
+aptable = Airport_Look_UP("us_airports.csv")
+
+aptable.getLatlong("ASE")
