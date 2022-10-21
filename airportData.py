e48cda30ea889d7be2c7d55a062295704c8d4242
Aiden-Jacobs <aiden192@gmail.com>
2022-10-21 00:00:48 -0700

    all airports

--- airportData.py
+++ airportData.py
@@ -3,8 +3,9 @@ import pandas as pd
 class Airport_Look_UP:
     def __init__(self, filename):
         self.file= pd.read_csv(filename)
-        self.file.columns = ["id","ident","type","name","latitude_deg","longitude_deg","elevation_ft","continent","country_name","iso_country","region_name","iso_region","local_region","municipality","scheduled_service","gps_code","iata_code","local_code","home_link","wikipedia_link","keywords","score","last_updated"]
-
+        self.file.columns = ["id","name","city","country_name","iata_code","icao_code","latitude_deg","longitude_deg","elevation_ft","Timezone","DST", "database_time_zone", "Type", "Source"]
+        #self.file.columns = ["id","ident","type","name","latitude_deg","longitude_deg","elevation_ft","continent","country_name","iso_country","region_name","iso_region","local_region","municipality","scheduled_service","gps_code","iata_code","local_code","home_link","wikipedia_link","keywords","score","last_updated"]
+        
     def getLatlong(self,iata_code):
         for i in range(self.file.index.size):
             if self.file.loc[i,"iata_code"] == iata_code:
@@ -12,6 +13,6 @@ class Airport_Look_UP:
 
     pass
 
-aptable = Airport_Look_UP("us_airports.csv")
+aptable = Airport_Look_UP("All_airports.csv")
 
 aptable.getLatlong("ASE")
