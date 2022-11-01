911cfa22de9f2c55bef57cc384a41a932a079f37
Aiden-Jacobs <aiden192@gmail.com>
2022-10-31 17:19:16 -0700

    Updated data

--- web_test2.py
+++ web_test2.py
@@ -1,3 +1,4 @@
+from multiprocessing.resource_sharer import stop
 from flask import Flask, render_template, request
 from datetime import datetime
 
@@ -8,7 +9,7 @@ import route
 Airports = airportData.Airport_Look_UP("us_airports.csv")#  All_airports.csv
 r = flightGen.flightGen()
 
-def getDepartureAP(FlightDataOb):
+def getLinkableAP(FlightDataOb):
     pointList = []
     apList = FlightDataOb.getDepartureAirports()
     for a in apList:
@@ -39,19 +40,53 @@ def getConnectionsPath(FlightDataOb,start,pl):
             pass
     #print("-----",f)
     return(out,routeList)
+
+
+def getConnectionsPatht(FlightDataOb,start,pl):
+    out = []
+    routeList = []
+    i = 0
+    f = route.Route_Manager(FlightDataOb.findFlightsTo(start.upper())).getRoutes() #modify findFlightsTo to return routes
+    for dest in f:
+        try:
+            lat = Airports.getLatlong(start.upper())[0]
+            long = Airports.getLatlong(start.upper())[1]
+            pl.append({'lat1' : lat, 'long1':long,
+            'lat2' : lat, 'long2':long,'Name':str(start.upper())})
+            out.append({'lat1' : lat, 'long1':long,
+                'lat2' : Airports.getLatlong(dest.getDepartureAirport())[0], 'long2':Airports.getLatlong(dest.getDepartureAirport())[1],'steps':2, 'flight': 1})
+            print(dest.getDepartureTime())
+            routeList.append({'Time':dest.GetFlightTime().total_seconds(),'Steps':1})
+            routeList[i]["Flights"] = {'flight1':str(str(dest.getDepartureTime().strftime("%H:%M:%S"))+" "+str(dest.getDepartureAirport())+"->"+str(dest.getArrivalAirport()))}
+            i += 1
+        except:
+            pass
+    #print("-----",f)
+    return(out,routeList)
     pass
 
 def valueGetTime(item):
     ti = item.get('Time')
     return(ti)
 
+def getFlightsToDraw(RtMnger,Stops):
+    pathList = []
+    for routes in RtMnger.filterLessThanNStops(Stops).sortByStops().getRoutes():#filterByStops
+        for flight in routes.getList():
+            pathList.append({'lat1' : Airports.getLatlong(flight.getDepartureAirport())[0], 'long1':Airports.getLatlong(flight.getDepartureAirport())[1],
+            'lat2' : Airports.getLatlong(flight.getArrivalAirport())[0], 'long2':Airports.getLatlong(flight.getArrivalAirport())[1],'steps':routes.getNumFlights(), 'flight': flight})
+        pass
+    return(pathList)
+
 def getPaths(FlightDataOb,Start, End,Stops):#add time check
     pathList = []
     routeList = []
     if FlightDataOb.checkValidStartEnd(Start, End):
-        FlightDataOb.findRoute(Start,End,datetime(2022,10,13,0,0,0),[])
+        FlightDataOb.findRoute(Start,End,datetime(2022,10,13,0,0,0),[], int(Stops))
         RtMnger = route.Route_Manager(r.getFoundRoutes())
         i = 0
+        
+        pathList = getFlightsToDraw(RtMnger.getUniqueRoutes(), Stops)
         for routes in RtMnger.filterLessThanNStops(Stops).sortByStops().getRoutes():#filterByStops
             #print(routes.getRouteTime())
             routeList.append({'Time':routes.getRouteTime().total_seconds(),'Steps':routes.getNumFlights()})
@@ -60,12 +95,9 @@ def getPaths(FlightDataOb,Start, End,Stops):#add time check
             for flight in routes.getList():
                 j += 1
                 routeList[i]["Flights"]["flight"+str(j)] = str(str(flight.getDepartureTime().strftime("%H:%M:%S"))+" "+str(flight.getDepartureAirport())+"->"+str(flight.getArrivalAirport()))
-                #print(Airports.getLatlong(flight.getDepartureAirport()))
-                #print(Airports.getLatlong(flight.getArrivalAirport()))
-                pathList.append({'lat1' : Airports.getLatlong(flight.getDepartureAirport())[0], 'long1':Airports.getLatlong(flight.getDepartureAirport())[1],
-                'lat2' : Airports.getLatlong(flight.getArrivalAirport())[0], 'long2':Airports.getLatlong(flight.getArrivalAirport())[1],'steps':routes.getNumFlights(), 'flight': flight})
             pass
             i += 1
+        
     #new_list = dict(sorted(routeList.items(), key=lambda item: item[1]))
     #print(routeList)
     return(pathList,routeList)
@@ -95,12 +127,16 @@ def root(Start = "SBP", End = "MSY", Stops = 2):
             if form_data['NumStopsReq'] != '':
                 Stops = form_data['NumStopsReq']
 
-        pointList = getDepartureAP(r)
+        pointList = getLinkableAP(r)
         RandP =  getPaths(r,Start, End, int(Stops))
         pathList = RandP[0]
         routes = RandP[1]
         if form_data['FromReq'] != '' and form_data['ToReq'] == '':
             RandP = getConnectionsPath(r, form_data['FromReq'],pointList)
+            pathList = RandP[0]
+            routes = RandP[1]
+        if form_data['FromReq'] == '' and form_data['ToReq'] != '':
+            RandP = getConnectionsPatht(r, form_data['ToReq'],pointList)
             pathList = RandP[0]
             routes = RandP[1]
 
