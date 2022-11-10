465c0a5468bba3d88d0d021c8f283b038da9c1cc
Aiden-Jacobs <aiden192@gmail.com>
2022-11-10 12:48:13 -0800

    reduced functions and cleaned up logic

--- web_test2.py
+++ web_test2.py
@@ -2,14 +2,18 @@ from multiprocessing.resource_sharer import stop
 from flask import Flask, render_template, request
 from datetime import datetime
 
-
 import airportData
 import flightGen
 import route
+
 Airports = airportData.Airport_Look_UP("us_airports.csv")#  All_airports.csv
 r = flightGen.flightGen()
 
 def getLinkableAP(FlightDataOb):
+    """
+    Returns pointList a list of dictionaries/Json of Points to be passed to the javascript
+        {lat1:Float,long1:Float,lat2:Float,long2:Float,Name:Str(iata_code)}
+    """
     pointList = []
     apList = FlightDataOb.getDepartureAirports()
     for a in apList:
@@ -19,91 +23,90 @@ def getLinkableAP(FlightDataOb):
         'lat2' : lat, 'long2':long,'Name':str(a)})
     return(pointList)
 
-def getConnectionsPath(FlightDataOb,start,pl):
-    out = []
+def getRoutesToDisp(RtMnger):
+    """
+    Returns routeList a list of dictionaries of dictionaries/Json of routes to a destination to be passed to the HTML&javascript to be displayed in a list
+                [{Time: Route Time in seconds, Steps: stops in Route, Flights:{flight1: str(Departure time iata_code->iata_code), flight_n: str(Departure time iata_code->iata_code)}},...] 
+    
+    RtMnger, is type Route_Manager
+    """
     routeList = []
-    i = 0
-    f = FlightDataOb.findFlightsFrom(start.upper())
-    for dest in f:
-        try:
-            lat = Airports.getLatlong(start.upper())[0]
-            long = Airports.getLatlong(start.upper())[1]
-            pl.append({'lat1' : lat, 'long1':long,
-            'lat2' : lat, 'long2':long,'Name':str(start.upper())})
-            out.append({'lat1' : lat, 'long1':long,
-                'lat2' : Airports.getLatlong(dest.getArrivalAirport())[0], 'long2':Airports.getLatlong(dest.getArrivalAirport())[1],'steps':2, 'flight': 1})
-            print(dest.getDepartureTime())
-            routeList.append({'Time':dest.GetFlightTime().total_seconds(),'Steps':1})
-            routeList[i]["Flights"] = {'flight1':str(str(dest.getDepartureTime().strftime("%H:%M:%S"))+" "+str(dest.getDepartureAirport())+"->"+str(dest.getArrivalAirport()))}
-            i += 1
-        except:
-            pass
-    #print("-----",f)
-    return(out,routeList)
-
-
-def getConnectionsPatht(FlightDataOb,start,pl):
-    out = []
-    routeList = []
-    i = 0
-    f = route.Route_Manager(FlightDataOb.findFlightsTo(start.upper())).getRoutes() #modify findFlightsTo to return routes
-    for dest in f:
-        try:
-            lat = Airports.getLatlong(start.upper())[0]
-            long = Airports.getLatlong(start.upper())[1]
-            pl.append({'lat1' : lat, 'long1':long,
-            'lat2' : lat, 'long2':long,'Name':str(start.upper())})
-            out.append({'lat1' : lat, 'long1':long,
-                'lat2' : Airports.getLatlong(dest.getDepartureAirport())[0], 'long2':Airports.getLatlong(dest.getDepartureAirport())[1],'steps':2, 'flight': 1})
-            print(dest.getDepartureTime())
-            routeList.append({'Time':dest.GetFlightTime().total_seconds(),'Steps':1})
-            routeList[i]["Flights"] = {'flight1':str(str(dest.getDepartureTime().strftime("%H:%M:%S"))+" "+str(dest.getDepartureAirport())+"->"+str(dest.getArrivalAirport()))}
-            i += 1
-        except:
-            pass
-    #print("-----",f)
-    return(out,routeList)
-    pass
-
-def valueGetTime(item):
-    ti = item.get('Time')
-    return(ti)
+    i = 0 
+    for routes in RtMnger.sortST().getRoutes():#filterByTime
+        routeList.append({'Time':routes.getRouteTime().total_seconds(),'Steps':routes.getNumFlights()})
+        routeList[i]["Flights"] = {}
+        j = 0
+        for flight in routes.getList():
+            j += 1
+            routeList[i]["Flights"]["flight"+str(j)] = str(str(flight.getDepartureTime().strftime("%H:%M:%S"))+" "+str(flight.getDepartureAirport())+"->"+str(flight.getArrivalAirport()))
+        pass
+        i += 1
+    return(routeList)
 
 def getFlightsToDraw(RtMnger,Stops):
+    """
+    Returns pathList a list of dictionaries/Json of lines/flights to be passed to the javascript
+            [{lat1:Float,long1:Float,lat2:Float,long2:Float,steps:Int,flight:Str},...]
+
+    RtMnger, is type Route_Manager
+        best practice to use Route_Manager.getUniqueRoutes()
+    Stops, is type Int
+    """
     pathList = []
     for routes in RtMnger.filterLessThanNStops(Stops).sortByStops().getRoutes():#filterByStops
-        for flight in routes.getList():
-            pathList.append({'lat1' : Airports.getLatlong(flight.getDepartureAirport())[0], 'long1':Airports.getLatlong(flight.getDepartureAirport())[1],
-            'lat2' : Airports.getLatlong(flight.getArrivalAirport())[0], 'long2':Airports.getLatlong(flight.getArrivalAirport())[1],'steps':routes.getNumFlights(), 'flight': flight})
-        pass
+        try:# try is to filter out airports not in database
+            for flight in routes.getList():
+                pathList.append({'lat1' : Airports.getLatlong(flight.getDepartureAirport())[0], 'long1':Airports.getLatlong(flight.getDepartureAirport())[1],
+                'lat2' : Airports.getLatlong(flight.getArrivalAirport())[0], 'long2':Airports.getLatlong(flight.getArrivalAirport())[1],'steps':routes.getNumFlights(), 'flight': flight})
+            pass
+        except:
+            pass
     return(pathList)
 
-def getPaths(FlightDataOb,Start, End,Stops):#add time check
+def textToDateTime(time):#todo change to be datetime.now
+    """Returns datetime modified for the selected filter time"""
+    try:
+        return(datetime(2022,10,19,int(time[0:2]),0,0))
+    except:
+        return(datetime(2022,10,13,0,0,0))
+
+def getPaths(FindFlightOb,Start, End, Time, Stops = 1):#add time check 
+    """
+    Returns a tuple (pathList,routeList)
+            pathList a list of dictionary/Json of lines/flights to be passed to the HTML&javascript
+                [{lat1:Float,long1:Float,lat2:Float,long2:Float,steps:Int,flight:Str},...]
+            routeList a list of dictionaries of dictionaries/Json of routes to a destination to be passed to the HTML&javascript to be displayed in a list
+                [{Time: Route Time in seconds, Steps: stops in Route, Flights:{flight1: str(Departure time iata_code->iata_code), flight_n: str(Departure time iata_code->iata_code)}},...] 
+    
+    FindFlightOb, is type Route_Finder for day of interest 
+    Start, is type Str of an iata_code
+    End, is type Str of an iata_code
+    Time, is type datetime 
+            used to get routes after it
+    Stops, is type Int
+    """
     pathList = []
     routeList = []
-    if FlightDataOb.checkValidStartEnd(Start, End):
-        FlightDataOb.findRoute(Start,End,datetime(2022,10,13,0,0,0),[], int(Stops))
-        RtMnger = route.Route_Manager(r.getFoundRoutes())
-        i = 0
-        
-        pathList = getFlightsToDraw(RtMnger.getUniqueRoutes(), Stops)
-        for routes in RtMnger.filterLessThanNStops(Stops).sortByStops().getRoutes():#filterByStops
-            #print(routes.getRouteTime())
-            routeList.append({'Time':routes.getRouteTime().total_seconds(),'Steps':routes.getNumFlights()})
-            routeList[i]["Flights"] = {}
-            j = 0
-            for flight in routes.getList():
-                j += 1
-                routeList[i]["Flights"]["flight"+str(j)] = str(str(flight.getDepartureTime().strftime("%H:%M:%S"))+" "+str(flight.getDepartureAirport())+"->"+str(flight.getArrivalAirport()))
-            pass
-            i += 1
-        
-    #new_list = dict(sorted(routeList.items(), key=lambda item: item[1]))
-    #print(routeList)
-    return(pathList,routeList)
+    Start = Start.upper()
+    End = End.upper()
+    if Start != '' and End != '':
+        if FindFlightOb.checkValidStartEnd(Start, End):
+            FindFlightOb.findRoute(Start,End,datetime(2022,10,13,0,0,0),[], int(Stops))
+    elif Start != '' and End == '' and FindFlightOb.checkValidAirport(Start):
+        FindFlightOb.findFlightsFrom(Start)
+    elif Start == '' and End != '' and FindFlightOb.checkValidAirport(End):
+        FindFlightOb.findFlightsTo(End)
+    else:
+        return([],[])
+    RtMnger = route.Route_Manager(FindFlightOb.getFoundRoutes())
+    RtMnger = RtMnger.filterByStartTime(Time)
+    pathList = getFlightsToDraw(RtMnger.getUniqueRoutes(), Stops)
+    routeList = getRoutesToDisp(RtMnger)
+    if Start == '' and End == '':
+        return([],[])
+    else:
+        return(pathList,routeList)
 
-def orderRoutes(routeList):
-    pass
 
 app=Flask(__name__)
 
@@ -111,13 +114,15 @@ app=Flask(__name__)
 #@app.route('/<Start>/<End>/<Stops>')
 @app.route('/<Start>/<End>/<Stops>',methods = ['POST', 'GET'])
 @app.route('/',methods = ['POST', 'GET'])
-def root(Start = "SBP", End = "MSY", Stops = 2):
+def root(Start = "", End = "", Stops = 1, filterTime = datetime(2022,10,13,0,0,0)): #todo switch to time.now 
     Start = Start.upper()
     End = End.upper()
     pathList = []
     pointList = []
+    pointList = getLinkableAP(r)
     routes = []
     markers = []
+    RandP = []
     if request.method == 'POST':
         form_data = request.form
         print(form_data)
@@ -126,28 +131,46 @@ def root(Start = "SBP", End = "MSY", Stops = 2):
             End = form_data['ToReq'].upper()
             if form_data['NumStopsReq'] != '':
                 Stops = form_data['NumStopsReq']
-
-        pointList = getLinkableAP(r)
-        RandP =  getPaths(r,Start, End, int(Stops))
+                #if type(Stops) !=  int:  todo check if works
+                #    Stops = 0
+            latlong1 = Airports.getLatlong(Start)
+            latlong2 = Airports.getLatlong(End)
+            if latlong1 != None and latlong2 != None:
+                markers.append({'lat':latlong1[0],
+                                'lon':latlong1[1],
+                                'popup':"Start "+Start})
+                markers.append({'lat':latlong2[0],
+                                'lon':latlong2[1],
+                                'popup':"End "+End})
+        if form_data['FromReq'] != '' and form_data['ToReq'] == '':
+            Start = form_data['FromReq'].upper()
+            latlong1 = Airports.getLatlong(Start)
+            if latlong1 != None:
+                markers.append({'lat':latlong1[0],
+                                'lon':latlong1[1],
+                                'popup':"Start "+Start})
+        if form_data['FromReq'] == '' and form_data['ToReq'] != '':
+            End = form_data['ToReq'].upper()
+            latlong2 = Airports.getLatlong(End)
+            if r.checkValidAirport(Start):
+                markers.append({'lat':latlong2[0],
+                                'lon':latlong2[1],
+                                'popup':"End "+End})
+        if form_data['TimeList'] != '':
+            filterTime = textToDateTime(form_data['TimeList'])
+        print(filterTime)
+        RandP =  getPaths(r,Start, End,filterTime, int(Stops))
         pathList = RandP[0]
         routes = RandP[1]
-        if form_data['FromReq'] != '' and form_data['ToReq'] == '':
-            RandP = getConnectionsPath(r, form_data['FromReq'],pointList)
-            pathList = RandP[0]
-            routes = RandP[1]
-        if form_data['FromReq'] == '' and form_data['ToReq'] != '':
-            RandP = getConnectionsPatht(r, form_data['ToReq'],pointList)
-            pathList = RandP[0]
-            routes = RandP[1]
 
-        markers=[
-            {'lat':Airports.getLatlong(Start)[0],
-            'lon':Airports.getLatlong(Start)[1],
-            'popup':"Start "+Start},
-            {'lat':Airports.getLatlong(End)[0],
-            'lon':Airports.getLatlong(End)[1],
-            'popup':"End "+End}
-            ]
+#        markers=[
+#            {'lat':Airports.getLatlong(Start)[0],
+#            'lon':Airports.getLatlong(Start)[1],
+#            'popup':"Start "+Start},
+#            {'lat':Airports.getLatlong(End)[0],
+#            'lon':Airports.getLatlong(End)[1],
+#            'popup':"End "+End}
+#            ]
     
     return render_template('index.html',markers=markers, latlongs = pathList, pointList = pointList, r = routes)
 
